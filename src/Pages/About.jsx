1e7e93f17d2a2aabcc131ce8e776f3bb7d5253b0
Nageshwar Nath Tripathi <nntivari11111@gmail.com>
2022-06-10 09:09:58 +0530

    routing project

---
+++ src/Pages/About.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div className="">
+        <div className="">
+      
+
+          <div className="">
+            <dl className="">
+              <div className="">
+                <dt>
+                  <div className="">
+                   
+                  </div>
+                  <p className="">
+                  
+                  </p>
+                </dt>
+                <dd className="">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Maiores impedit perferendis suscipit eaque, iste dolor
+                  cupiditate blanditiis ratione.
+                </dd>
+              </div>
+
+        
+
+              <div className="">
+                <dt>
+                  <div className="">
+                    
+                  </div>
+                  <p className="">
+                   
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Maiores impedit perferendis suscipit eaque, iste dolor
+                  cupiditate blanditiis ratione.
+                </dd>
+              </div>
+              <div className="relative mt-10">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 10V3L4 14h7v7l9-11h-7z"
+                      />
+                    </svg>
+                  </div>
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                  Trendy women’s clothing &  Smart men’s clothing and  Beauty begins here
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Maiores impedit perferendis suscipit eaque, iste dolor
+                  cupiditate blanditiis ratione.
+                </dd>
+              </div>
+              <div className="relative mt-10">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                      />
+                    </svg>
+                  </div>
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                    Fun and frolic & Fashionable footwear
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Maiores impedit perferendis suscipit eaque, iste dolor
+                  cupiditate blanditiis ratione.
+                </dd>
+              </div>
+
+              <div className="relative mt-10">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <svg
+                      className="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                      />
+                    </svg>
+                  </div>
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                    Stylish accessories
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Maiores impedit perferendis suscipit eaque, iste dolor
+                  cupiditate blanditiis ratione.
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Logo, NavbarDiv } from "../Components/Main.styled";
+import { Link, useNavigate } from "react-router-dom";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <NavbarDiv>
+        <div>
+          <Logo>
+            <Link to={"/"}>ROUTER PROJECT</Link>
+          </Logo>
+        </div>
+        <div className="links">
+          <Link to={"/"}>Home</Link>
+          <Link to={"/products"}>Products</Link>
+          <Link to={"/about"}>About Us</Link>
+          <Link to={"/contact"}>Contact Us</Link>
+          <ShoppingBagIcon onClick={() => navigate("/shoppingcart")} />
+        </div>
+      </NavbarDiv>
+    </>
+  );
+};
+
+export default Navbar;
